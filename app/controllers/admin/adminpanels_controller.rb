c31962510894389a5793fcf9ebb2c69942de1278
Pulitzier <ybrv2010@gmail.com>
2017-08-19 21:17:28 +0300

    Preparing for resque job

--- app/controllers/admin/adminpanels_controller.rb
+++ app/controllers/admin/adminpanels_controller.rb
@@ -1,12 +1,12 @@
 class Admin::AdminpanelsController < ApplicationController
 
 	def index
-		@adminpanels = Adminpanel.all
-		render nothing: true
+		# @adminpanels = Adminpanel.all
 	end
 
 	def create
 		@adminpanel = Adminpanel.create(frequency: params[:frequency], date: params[:date], time: params[:time])
+		redirect_to "/admin"
 	end
 
 end
---
+++ app/jobs/download_info_job.rb
@@ -0,0 +1,6 @@
+# class DownloadInfoJob < ApplicationJob
+# 	queue_as :default
+ 
+#   def perform()
+#   end
+# end
