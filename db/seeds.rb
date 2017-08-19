732e09f0bcc19d3d37d1cddb15f3084a1fb2886c
Pulitzier <ybrv2010@gmail.com>
2017-08-19 22:38:59 +0300

    Seed heroku db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dashboard.create(update_date: '2013-03-03 00:00:00',
+	carousel_info: 'There should be carousel',
+	newbie: 'There goes New',
+	others: 'There are others')
